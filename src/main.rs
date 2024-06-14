5b01535be5adc388be7b647d2969ca2c38148bf8
Anurag Kumar Singh <singh.ask@outlook.com>
2024-06-14 17:29:54 +0530

    stage: Body return response config

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
-use std::io::Read;
-use std::io::Write;
-use std::net::TcpListener;
-use std::net::TcpStream;
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let _response = "HTTP/1.1 200 OK\r\n\r\n";
-    let get = b"GET / HTTP/1.1\r\n";
-    // Represents the HTTP response based on the contents of the buffer.
-    let response = if buffer.starts_with(get) {
-        "HTTP/1.1 200 OK\r\n\r\n"
-    } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n"
-    };
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
+use std::{
+    io::{Read, Result, Write},
+    net::{TcpListener, TcpStream},
+};
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    println!("Anurag's HTTP Server!");
+    // Uncomment this block to pass the first stage
+    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    //
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
-                handle_connection(_stream);
+                handle_connection(_stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    println!("Connection established!");
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let lines: Vec<&str> = request.split("\r\n").collect();
+    let tokens: Vec<&str> = lines[0].split(" ").collect();
+    match tokens[0] {
+        "GET" => {
+            if tokens[1] == "/" {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+            } else if tokens[1].starts_with("/echo/") {
+                let response = tokens[1].replace("/echo/", "");
+                stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", response.len(), response).as_bytes())?;
+            } else {
+                stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")?;
+            }
+        }
+        _ => {
+            println!("Unknown method: {}", tokens[0])
+        }
+    }
+    Ok(())
 }
