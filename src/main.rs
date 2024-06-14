ad415cb13cd7dd23872eca2605205b06e49db07d
Anurag Kumar Singh <singh.ask@outlook.com>
2024-06-14 17:25:16 +0530

    stage: CRLF URL extraction implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
+use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
-
+use std::net::TcpStream;
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let _response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+    // Represents the HTTP response based on the contents of the buffer.
+    let response = if buffer.starts_with(get) {
+        "HTTP/1.1 200 OK\r\n\r\n"
+    } else {
+        "HTTP/1.1 404 Not Found\r\n\r\n"
+    };
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
